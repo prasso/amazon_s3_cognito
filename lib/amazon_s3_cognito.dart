7cd625641f2b1c942a24233eb18d51da1a2ea85b
bperreault <bcp@faxt.com>
2020-12-26 11:07:08 -0500

    updates so iOS will work with XCode 12

--- lib/amazon_s3_cognito.dart
+++ lib/amazon_s3_cognito.dart
@@ -25,16 +25,23 @@ class AmazonS3Cognito {
 
   static Future<String> upload(String filepath, String bucket, String identity,
       String imageName, String region, String subRegion) async {
-    final Map<String, dynamic> params = <String, dynamic>{
-      'filePath': filepath,
-      'bucket': bucket,
-      'identity': identity,
-      'imageName': imageName,
-      'region': region,
-      'subRegion': subRegion
-    };
-    final String imagePath = await _channel.invokeMethod('uploadImage', params);
-    return imagePath;
+    print('Region: ' + region + ' subRegion: ' + subRegion);
+    try {
+      final Map<String, dynamic> params = <String, dynamic>{
+        'filePath': filepath,
+        'bucket': bucket,
+        'identity': identity,
+        'imageName': imageName,
+        'region': region,
+        'subRegion': subRegion
+      };
+      final String imagePath =
+          await _channel.invokeMethod('uploadImage', params);
+      return imagePath;
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+    return '';
   }
 
   static Future<String> delete(String bucket, String identity, String imageName,
@@ -48,5 +55,32 @@ class AmazonS3Cognito {
     };
     final String imagePath = await _channel.invokeMethod('deleteImage', params);
     return imagePath;
+  }
+
+  static Future<List<String>> listFiles(
+      String bucket,
+      String identity,
+      String prefix,
+      String region,
+      String subRegion,
+      String cloudFrontWebUrl) async {
+    final Map<String, dynamic> params = <String, dynamic>{
+      'bucket': bucket,
+      'identity': identity,
+      'prefix': prefix,
+      'region': region,
+      'subRegion': subRegion
+    };
+    List<String> files = new List();
+    try {
+      List<dynamic> keys = await _channel.invokeMethod('listFiles', params);
+      for (String key in keys) {
+        files.add("$cloudFrontWebUrl$bucket/$key");
+      }
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    return files;
   }
 }
